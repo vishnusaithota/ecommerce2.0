7a2718a4a5fa64afa8f7b45afbc1a4855f6518e3
tnvrravish <vt7@njit.edu>
2023-05-30 00:56:58 -0400

    reset commmit

--- order-service/src/main/java/com/webservices/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/webservices/orderservice/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.webservices.orderservice.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,8 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    @LoadBalanced
-    public WebClient.Builder webClientBuilder(){
-        return WebClient.builder();
+    public WebClient webClient(){
+        return WebClient.builder().build();
     }
 }
