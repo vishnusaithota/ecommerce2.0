a3338148a8e1770d1b37eb7e3cfc8dcf545bf307
tnvrravish <vt7@njit.edu>
2023-06-01 00:13:31 -0400

    reset fix with no security implemented

--- api-gateway/src/main/java/com/webservices/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/webservices/apigateway/config/SecurityConfig.java
@@ -1,27 +1,28 @@
 package com.webservices.apigateway.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-@EnableWebFluxSecurity
+//@Configuration
+//@EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+//
+//        serverHttpSecurity.csrf()
+//                .disable()
+//                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers("/eureka/**","/api/product")
+//                        .permitAll()
+//                        .anyExchange().authenticated())
+//                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+//        return serverHttpSecurity.build();
 
-        serverHttpSecurity.csrf()
-                .disable()
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/eureka/**")
-                        .permitAll()
-                        .anyExchange().authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        return serverHttpSecurity.build();
 
-
-    }
+//    }
 }
