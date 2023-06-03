f097df031f6f7849db53ec8782f9752605638892
tnvrravish <vt7@njit.edu>
2023-06-03 01:01:32 -0400

    fixed the keycloak issues as it was due to the docker version and time difference

--- api-gateway/src/main/java/com/webservices/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/webservices/apigateway/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.webservices.apigateway.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -11,13 +12,14 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
-        serverHttpSecurity.csrf()
-                .disable()
-                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
-                        .permitAll()
-                        .anyExchange()
-                        .authenticated())
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf().disable()
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
         return serverHttpSecurity.build();
     }
