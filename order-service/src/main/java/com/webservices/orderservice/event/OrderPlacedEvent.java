6e6f7ac17d84aa48ac451287a9825b3b10fa01be
tnvrravish <vt7@njit.edu>
2023-06-07 23:18:12 -0400

    kafka set up completed and properties added and docker compose added and notification service called

---
+++ order-service/src/main/java/com/webservices/orderservice/event/OrderPlacedEvent.java
@@ -0,0 +1,16 @@
+package com.webservices.orderservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+
+    private String orderNumber;
+
+}
--- order-service/src/main/java/com/webservices/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/webservices/orderservice/service/OrderService.java
@@ -3,12 +3,15 @@ package com.webservices.orderservice.service;
 import com.webservices.orderservice.dto.InventoryResponse;
 import com.webservices.orderservice.dto.OrderLineItemsDto;
 import com.webservices.orderservice.dto.OrderRequest;
+import com.webservices.orderservice.event.OrderPlacedEvent;
 import com.webservices.orderservice.model.Order;
 import com.webservices.orderservice.model.OrderLineItems;
 import com.webservices.orderservice.respository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -28,6 +31,8 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     private final WebClient.Builder webClientBuilder;
+
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     private final Tracer tracer;
 
@@ -56,6 +61,7 @@ public class OrderService {
 
             if (allProductsInStock){
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic",new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order Placed Successfully";
             }else {
                 throw new IllegalArgumentException("Product is not in stock, Please try again later");
